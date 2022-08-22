90b5cfa8e551f46a4db2659d8114321d63026a63
Prajakta Deokule <70521908+Prajakta456@users.noreply.github.com>
2022-08-22 09:08:04 +0530

    Update RoleRepo.java

--- java/com/employee/employeeManagement/repository/RoleRepo.java
+++ java/com/employee/employeeManagement/repository/RoleRepo.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.employee.employeeManagement.entity.Role;
+/*CrudRepository implements basic CRUD operations, including count, delete, deleteById, save, saveAll, findById, and findAll.*/
 
 @Repository
 public interface RoleRepo extends CrudRepository<Role,Integer>{
