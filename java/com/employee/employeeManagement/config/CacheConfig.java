9c95a229db879911da9cf03f32d4c19b3598af08
https://github.com/Prajakta456 <009prajakta@gmail.com>
2026-09-09 22:43:55 +0530

    edited to fix this

---
+++ java/com/employee/employeeManagement/config/CacheConfig.java
@@ -0,0 +1,25 @@
+package com.employee.employeeManagement.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+
+        return new ConcurrentMapCacheManager(
+                "employees",
+                "roles",
+                "leaveInformation",
+                "employeeLeave",
+                "employeeLeaves",
+                "applyLeaveById"
+        );
+    }
+}
